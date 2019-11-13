eafa4c1bb9c8d3e5000aefa13a4413ff06c3bb1d
Yazan Asad <yazan_asad@hotmail.co.uk>
2019-11-14 07:52:54 +0800

    portfolio site

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import yazpicture from "../images/yazan picture office.jpg";
 
 class Landing extends Component {
   render() {
-    return(
-      <div><h1>Landing Page</h1></div>
-    )
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img src={yazpicture} alt="me" className="avatar-img" />
+            <div className="banner-text">
+              <h1>Front End Developer</h1>
+              <hr />
+              <p>HTML/CSS || Javascript || React || NodeJS </p>
+              <div className="social-links">
+                {/*LinkedIn */}
+                <a
+                  href="https://www.google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />{" "}
+                </a>
+
+                {/*Github */}
+                <a
+                  href="https://www.google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />{" "}
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
   }
 }
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,127 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png) center / cover"
+              }}
+            >
+              React Project 1
+            </CardTitle>
+            <CardText>dummy text goes here</CardText>
+            <CardActions className="card-actions" border>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png) center / cover"
+              }}
+            >
+              React Project 1
+            </CardTitle>
+            <CardText>dummy text goes here</CardText>
+            <CardActions className="card-actions" border>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png) center / cover"
+              }}
+            >
+              React Project 1
+            </CardTitle>
+            <CardText>dummy text goes here</CardText>
+            <CardActions className="card-actions" border>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+              <Button colored>github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>this is html/jss</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>this is react</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab="this.state.activeTab"
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>HTML/CSS</Tab>
+          <Tab>React</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
